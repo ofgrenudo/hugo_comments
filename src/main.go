0f00529a1ae6b3fda3dfcae8816c40300ed143b4
Joshua Winters-Brown <jwintersbro@Joshuas-MacBook-Air.local>
2025-08-14 20:56:37 -0400

    fix: archive db string for later

--- src/main.go
+++ src/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// Example of a database connection string
+	// "postgres://${DB_USER}:${DB_PASSWORD}@db:${DB_PORT}/${DB_NAME}?sslmode=disable"
 	fmt.Println("Hello World!")
 }
